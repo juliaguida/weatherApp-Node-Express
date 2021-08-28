b1bb6e66dd145d1c4ac88a74818861109ac2d396
Julia Guida <julia.guida@FT-MW5760.homenet.telecomitalia.it>
2021-08-28 18:48:35 +0100

    node express weatherApp

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
+
+//code below the same as import request in Python however in Js it use a const variable.
 const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
 
 const port = process.env.PORT || 3000
+//It will return the path and backward ..\
+const publicStaticDirectPath = path.join(__dirname, 'public')
 
 app.get('', (req, res) => {
     res.send("Hi this is weather app");
